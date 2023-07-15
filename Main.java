9c1b12f9b21ce2fc9db7ee3715888bec8c5414cc
OlegMalanchuk <126597034+OlegMalanchuk@users.noreply.github.com>
2023-07-15 22:20:03 +0300

    secound commit

--- Main.java
+++ Main.java
@@ -1,5 +1,29 @@
-public class Main {
-    public static void main(String[] args) {
-        
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("ddd");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
